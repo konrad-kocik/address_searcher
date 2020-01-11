bf6d37d405b981eaf01906b138b464ef07a20481
Konrad Kocik <konrad.kocik@gmail.com>
2020-01-11 19:08:31 +0100

    Add early version of finding duplicates

---
+++ nicelka/find_duplicates.py
@@ -0,0 +1,82 @@
+from os import path
+
+
+def find_duplicates(report_file_name):
+    results = _get_results_from_report(report_file_name)
+    duplicates = _get_duplicates_from_results(results)
+    _save_duplicates(duplicates)
+    _show_duplicates(duplicates)
+
+
+def _get_results_from_report(file_name):
+    results = []
+
+    with open(path.join('data', file_name), encoding='utf8') as file:
+        result = []
+
+        for line in file:
+            line = line.strip()
+            if _is_line_empty_in_result(line, result):
+                continue
+            elif _is_line_empty_between_results(line, result):
+                results = _add_to_results(results, result)
+                result = []
+            else:
+                result.append(line)
+
+    return results
+
+
+def _is_line_empty_in_result(line, result):
+    return not line and len(result) < 3
+
+
+def _is_line_empty_between_results(line, result):
+    return not line and len(result) >= 3
+
+
+def _add_to_results(results, result):
+    if len(result) == 4:
+        result[0] = result[0] + " " + result.pop(1)
+    elif len(result) > 4:
+        raise Exception("Incorrect result: {}".format(result))
+    results.append(result)
+
+    return results
+
+
+def _get_duplicates_from_results(results):
+    duplicates = []
+
+    for i, result in enumerate(results):
+        for other in results[i+1:]:
+            if result[1] == other[1] and result[2] == other[2]:
+                if result not in duplicates:
+                    duplicates.append(result)
+                if other not in duplicates:
+                    duplicates.append(other)
+                break
+
+    duplicates.sort(key=lambda x: x[1] + x[2])
+    return duplicates
+
+
+def _show_duplicates(duplicates):
+    for duplicate_id, duplicate in enumerate(duplicates):
+        print("Duplikat nr {}".format(duplicate_id + 1))
+        print(duplicate[0])
+        print(duplicate[1])
+        print(duplicate[2])
+        print('')
+
+
+def _save_duplicates(duplicates):
+    with open('duplicates.txt', mode='w', encoding='utf8') as file:
+        for duplicate_id, duplicate in enumerate(duplicates):
+            file.write("Duplikat nr {}".format(duplicate_id + 1) + '\n')
+            file.write(duplicate[0] + '\n')
+            file.write(duplicate[1] + '\n')
+            file.write(duplicate[2] + '\n\n')
+
+
+find_duplicates('report_with_duplicates.txt')
