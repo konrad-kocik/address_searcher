f9900733a9ea59e37b34bb1cd98ff7964da30e80
Konrad Kocik <konrad.kocik@gmail.com>
2020-01-21 20:40:49 +0100

    Add getting black list to Gateway

--- nicelka/gateway/gateway.py
+++ nicelka/gateway/gateway.py
@@ -10,19 +10,26 @@ class Gateway:
         self._data_dir_path = data_dir_path
         self._cities_file_path = self._assemble_data_file_path('cities.txt')
         self._keys_file_path = self._assemble_data_file_path('keys.txt')
+        self._black_list_filer_path = self._assemble_data_file_path('black_list.txt')
 
         Logger.info(self, 'Cities file path: {}'.format(self._cities_file_path))
         Logger.info(self, 'Keys file path: {}'.format(self._keys_file_path))
 
     def get_cities(self):
         Logger.info(self, 'Getting cities...')
-        with open(self._cities_file_path, encoding=self._FILE_ENCODING) as file:
-            return [city.strip() for city in file.readlines() if city.strip()]
+        return self._get_items_from_file(self._cities_file_path)
 
     def get_keys(self):
         Logger.info(self, 'Getting keys...')
-        with open(self._keys_file_path, encoding=self._FILE_ENCODING) as file:
-            return [key.strip() for key in file.readlines() if key.strip()]
+        return self._get_items_from_file(self._keys_file_path)
+
+    def get_black_list(self):
+        Logger.info(self, 'Getting black list...')
+        return self._get_items_from_file(self._black_list_filer_path)
 
     def _assemble_data_file_path(self, file_name):
         return path.join(self._data_dir_path, file_name)
+
+    def _get_items_from_file(self, file_path):
+        with open(file_path, encoding=self._FILE_ENCODING) as file:
+            return [item.strip() for item in file.readlines() if item.strip()]
