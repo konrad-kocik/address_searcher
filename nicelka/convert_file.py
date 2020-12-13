bcee870df492034d393f4a06ce900a78dfd8b3d6
Konrad Kocik <konrad.kocik@gmail.com>
2020-12-13 16:37:13 +0100

    Add workbook and sheet names to convert_file.py

--- nicelka/convert_file.py
+++ nicelka/convert_file.py
@@ -2,5 +2,7 @@ from nicelka.data_normalizer.importer import import_results_from_text_file
 from nicelka.data_normalizer.exporter import export_results_to_excel_file
 
 
-results = import_results_from_text_file('data', 'report_with_duplicates.txt')
-export_results_to_excel_file(results, 'data', 'report.xls')
+name = 'Oświęcim'
+
+results = import_results_from_text_file('data', 'report_text_file.txt')
+export_results_to_excel_file(results, 'data', '{}.xls'.format(name), name)
